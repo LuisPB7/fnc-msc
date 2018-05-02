34bf517715ee3d00ad9643fcb1ccb8dd321e3251
Luís Pedro Borges <luispb7@hotmail.com>
2018-05-02 16:38:41 +0100

    Update snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -75,8 +75,10 @@ class SnapshotCallbackBuilder:
         return callback_list
 
     def _cosine_anneal_schedule(self, t):
-        cos_inner = np.pi * (t % (self.T // self.M))  # t - 1 is used when t has 1-based indexing.
-        cos_inner /= self.T // self.M
-        cos_out = np.cos(cos_inner) + 1
-        return float(self.alpha_zero / 2 * cos_out)
+        if t % (self.T // self.M) != 0 and t < 20:
+            cos_inner = np.pi * (t % (self.T // self.M))  # t - 1 is used when t has 1-based indexing.
+            cos_inner /= self.T // self.M
+            cos_out = np.cos(cos_inner) + 1
+            return float(self.alpha_zero / 2 * cos_out)
+        return self.model.lr.get_value()
 
